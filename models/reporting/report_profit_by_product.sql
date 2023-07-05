244bd30769f98486d4b75b4acca8f2adf76d7487
SUVENDU BEURA <suvendu.beura@gmail.com>
2023-07-05 10:18:35 +0000

    devloping report profit by product

---
+++ models/reporting/report_profit_by_product.sql
@@ -0,0 +1,13 @@
+Select 
+     productid
+    , productname
+    , category
+    , subcategory
+    , sum(orderprofit) as profit
+from {{ ref('stg_orders') }}
+group by
+     productid
+    , productname
+    , category
+    , subcategory 
+order by profit desc
